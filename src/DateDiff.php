d013e4cc882d9c2127d6bbbe0609c21cabc7eb92
Grzegorz Firlej <dev@grzegorzfirlej.pl>
2017-06-16 00:33:49 +0200

    DateDiff class

---
+++ src/DateDiff.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Financial;
+
+
+final class DateDiff
+{
+    private $startDate;
+    private $endDate;
+
+    public function __construct(\DateTime $startDate, \DateTime $endDate)
+    {
+        $this->startDate = $startDate->setTime(0, 0, 0);
+        $this->endDate = $endDate->setTime(0, 0, 0);
+    }
+
+    public function days(): int
+    {
+        return ceil($this->endDate->diff($this->startDate)->days);
+    }
+
+    public function weeks(): int
+    {
+        return ceil(($this->endDate->diff($this->startDate)->days) / 7);
+    }
+
+    public function months(): int
+    {
+        return ((int)$this->endDate->format('Y') - (int)$this->startDate->format('Y')) * 12
+            + ((int)$this->endDate->format('m') - (int)$this->startDate->format('m'));
+    }
+
+    public function years(): int
+    {
+        return $this->endDate->diff($this->startDate)->y;
+    }
+}
+
