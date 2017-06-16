532fd5565a6bdc4b5f8ce81ddf8534e94bebb885
Grzegorz Firlej <dev@grzegorzfirlej.pl>
2017-06-16 16:45:34 +0200

    PaymentList has list of currencies of payments

--- src/PaymentList.php
+++ src/PaymentList.php
@@ -9,16 +9,23 @@ class PaymentList
 {
     /** @var ArrayCollection */
     private $list;
+    /** @var ArrayCollection */
+    private $currencies;
 
     public function __construct()
     {
         $this->list = new ArrayCollection;
+        $this->currencies = new ArrayCollection;
     }
 
     public function addPayment(Payment $payment) :PaymentList
     {
         $this->list->add($payment);
 
+        $currency = $payment->getValue()->getCurrency()->getCode();
+        if(!$this->currencies->contains($currency)) {
+            $this->currencies->add($currency);
+        }
         return $this;
     }
 
@@ -27,4 +34,8 @@ class PaymentList
         return $this->list;
     }
 
+    public function getCurrencies(): ArrayCollection
+    {
+        return $this->currencies;
+    }
 }
--- tests/Tests/PaymentListTest.php
+++ tests/Tests/PaymentListTest.php
@@ -10,12 +10,12 @@ use PHPUnit\Framework\TestCase;
 
 final class PaymentListTest extends TestCase
 {
-    function __construct($name = null, array $data = [], $dataName = '')
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
     }
 
-    function test_payment_list_values_count()
+    public function test_payment_list_values_count()
     {
 
         $paymentList = new PaymentList();
@@ -24,9 +24,15 @@ final class PaymentListTest extends TestCase
 
         $paymentList->addPayment(new Payment(Money::EUR(200), new \DateTime()));
         $this->assertEquals($paymentList->getList()->count(), 1);
+        $this->assertEquals($paymentList->getCurrencies()->count(), 1);
 
         $paymentList->addPayment(new Payment(Money::PLN(300), new \DateTime()));
         $this->assertEquals($paymentList->getList()->count(), 2);
+        $this->assertEquals($paymentList->getCurrencies()->count(), 2);
+
+        $paymentList->addPayment(new Payment(Money::EUR(427), new \DateTime()));
+        $this->assertEquals($paymentList->getList()->count(), 3);
+        $this->assertEquals($paymentList->getCurrencies()->count(), 2);
     }
 
 }
