793f248589751e61747ece6b18afce7fa0c82f3f
Grzegorz Firlej <dev@grzegorzfirlej.pl>
2017-10-31 19:18:54 +0100

    addidional tests

--- tests/Tests/FinancialTest.php
+++ tests/Tests/FinancialTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Financial;
 
-
 use Financial\Financial;
 use Financial\Payment;
 use Financial\PaymentList;
@@ -14,7 +13,6 @@ final class FinancialTest extends TestCase
 {
     public function test_xirr()
     {
-
         $paymentList = new PaymentList();
         $paymentList->addPayment(new Payment(Money::EUR(100), new \DateTime('2016-01-01')));
         $paymentList->addPayment(new Payment(Money::EUR(-120), new \DateTime('2016-12-31')));
@@ -22,8 +20,31 @@ final class FinancialTest extends TestCase
 
         $financial = new Financial();
         $this->assertFinite($financial->XIRR($paymentList));
-        $this->assertGreaterThanOrEqual(abs($financial->XIRR($paymentList) - 0.2), 0.002);
-
+        $this->assertEquals(0.2, round($financial->XIRR($paymentList), 4));
     }
 
+    public function test_xirr_if_investition_doubled()
+    {
+        $paymentList = new PaymentList();
+        $paymentList->addPayment(new Payment(Money::EUR(100), new \DateTime('2016-01-01')));
+        $paymentList->addPayment(new Payment(Money::EUR(-200), new \DateTime('2016-12-31')));
+        $paymentList = $paymentList->recalculateToGivenCurrency(new Currency('EUR'));
+
+        $financial = new Financial();
+        $this->assertFinite($financial->XIRR($paymentList));
+        $this->assertEquals(1, round($financial->XIRR($paymentList), 4));
+    }
+
+    public function test_xirr_if_investition_zeroed()
+    {
+        $paymentList = new PaymentList();
+        $paymentList->addPayment(new Payment(Money::EUR(100), new \DateTime('2016-01-01')));
+        $paymentList->addPayment(new Payment(Money::EUR(100), new \DateTime('2016-07-01')));
+        $paymentList->addPayment(new Payment(Money::EUR(-200), new \DateTime('2016-12-31')));
+        $paymentList = $paymentList->recalculateToGivenCurrency(new Currency('EUR'));
+
+        $financial = new Financial();
+        $this->assertFinite($financial->XIRR($paymentList));
+        $this->assertEquals(0, round($financial->XIRR($paymentList), 4));
+    }
 }
